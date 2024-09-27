6810aafb36b0424ffeebd254ec95f822d4e9cdf1
Lohith-kumar-11 <lohithkumarr0110@gmail.com>
2024-09-27 15:39:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+document.querySelector(".P1").addEventListener("click", function ()
+{  
+    window.open("https://www.bmw-motorrad.in/en/models/sport/s1000rr.html", "_blank")
+    // location.href  = "https://www.bmw-motorrad.in/en/models/sport/s1000rr.html";
+    
+});
+
+document.querySelector(".C1").addEventListener("click", function ()
+{  
+    window.open("https://www.bmw-motorrad.in/en/models/sport/s1000rr/accessories.html#/section-12a601a7", "_blank")
+    // location.href  = "https://www.bmw-motorrad.in/en/models/sport/s1000rr.html";
+    
+});
+
+document.querySelector(".P22").addEventListener("click", function ()
+{  
+    window.open("https://cars.mclaren.com/en/750s", "_blank")
+    // location.href  = "https://www.bmw-motorrad.in/en/models/sport/s1000rr.html";
+    
+});
+
+document.querySelector(".C2").addEventListener("click", function ()
+{  
+    window.open("https://configurator.mclaren.com/mclaren-ui/?profile/750S", "_blank")
+    // location.href  = "https://www.bmw-motorrad.in/en/models/sport/s1000rr.html";
+    
+});
+
+document.querySelector(".P3").addEventListener("click", function ()
+{  
+    window.open("https://www.ferrari.com/en-EN/auto/sf90-spider", "_blank")
+    // location.href  = "https://www.bmw-motorrad.in/en/models/sport/s1000rr.html";
+    
+});
+
+document.querySelector(".C3").addEventListener("click", function ()
+{  
+    window.open("https://carconfigurator.ferrari.com/en_EN/ferrari_car_configurator/sf90spider/fiorano/step/CLR?configuration=ee2591621e5f3dd9477bf79ed7fef2e60176e5e331de27419e8f78f5c2de4103", "_blank")
+    // location.href  = "https://www.bmw-motorrad.in/en/models/sport/s1000rr.html";
+    
+});
